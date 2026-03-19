7d5007c32f9a683bf1bfd870e5de6f71a8c58daf
IVANNIA VANESSA QUESADA BOGANTES <iquesadab@ucenfotec.ac.cr>
2026-03-19 14:46:18 -0600

    [feat]: crear clase Cliente con lista de productos y manejo de prioridad

--- ProyectoEstructurasdeDatos-main/ProyectoEstructurasdeDatos/src/clientes/Cliente.java
+++ ProyectoEstructurasdeDatos-main/ProyectoEstructurasdeDatos/src/clientes/Cliente.java
@@ -1,13 +1,85 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package clientes;
 
-/**
- *
- * @author Ivannia Quesada
- */
+import listaProductos.ListaProductos;
+
+
 public class Cliente {
     
+    // Atributos
+    private String nombre;
+    private String idCliente;
+    private int prioridad;
+    private ListaProductos carrito;
+    private Cliente siguiente;
+
+    // Metodos
+    // Constructor
+    public Cliente(String nombre, String idCliente, int prioridad) {
+        this.nombre = nombre;
+        this.idCliente = idCliente;
+        this.prioridad = prioridad;
+        carrito = new ListaProductos();
+        this.siguiente = null;
+        
+    }
+    
+    
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public ListaProductos getCarrito() {
+        return carrito;
+    }
+
+    public Cliente getSiguiente() {
+        return siguiente;
+    }
+    
+    
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public void setSiguiente(Cliente siguiente) {
+        this.siguiente = siguiente;
+    }
+    
+    
+    // toString
+    @Override
+    public String toString() {
+        String msjPrioridad;
+                
+        if(prioridad == 1){
+            msjPrioridad = "Básico";
+        } else if (prioridad == 2){
+            msjPrioridad = "Afiliado";
+        } else{
+            msjPrioridad = "Premium";
+        }
+
+        return "Cliente: " + nombre + "\n" +
+           "ID: " + idCliente + "\n" +
+           "Prioridad: " + msjPrioridad + "\n" +
+           "Carrito:\n" + carrito + "\n";
+    }
 }
