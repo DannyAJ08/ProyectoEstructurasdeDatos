b137e4c867bcb57981c4ee95aafd9d2e1c85d7db
IVANNIA VANESSA QUESADA BOGANTES <iquesadab@ucenfotec.ac.cr>
2026-03-22 19:45:34 -0600

    [refactor]: Corrección de código para correcta implementación del Menú

--- ProyectoEstructurasdeDatos-main/ProyectoEstructurasdeDatos/src/proyectoestructurasdedatos/ProyectoEstructurasdeDatos.java
+++ ProyectoEstructurasdeDatos-main/ProyectoEstructurasdeDatos/src/proyectoestructurasdedatos/ProyectoEstructurasdeDatos.java
@@ -1,6 +1,9 @@
 package proyectoestructurasdedatos;
-import listaProductos.ListaProductos;
+
 import listaProductos.NodoProducto;
+import clientes.Cliente;
+import tienda.Tienda;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,19 +11,20 @@ public class ProyectoEstructurasdeDatos {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // Scanner permite leer lo que escribe el usuario en consola
-        ListaProductos lista = new ListaProductos(); // Creamos la lista enlazada donde se guardarán los libros
+        
+        Tienda tienda = new Tienda("Librería Online", "Costa Rica");
+        
         int opcion; // Variable para el switch del menú principal
         boolean continuar = true; // Variable para controlar el ciclo while principal
         
         // Bucle principal del menú
         while(continuar){
             System.out.println("***** MENÚ LIBRERÍA *****\n" +
-                                "1. Insertar libro al inicio\n" +
-                                "2. Insertar libro al final\n" +
-                                "3. Mostrar libros\n" +
-                                "4. Buscar libro por ID\n" +
-                                "5. Eliminar libro\n" +
-                                "6. Modificar libro\n" +
+                                "1. Agregar producto al inventario\n" +
+                                "2. Mostrar inventario\n" +
+                                "3. Agregar cliente a la cola y comprar productos\n" +
+                                "4. Ver cliente al frente\n" +
+                                "5. Atender siguiente cliente\n" +
                                 "0. Salir\n");
 
             System.out.print("Seleccione una opción: \n");
@@ -29,16 +33,26 @@ public class ProyectoEstructurasdeDatos {
             
             switch(opcion){
                 
-                // Insertar al inicio
+                // Agregar producto al inventario
                 case 1:
-                    System.out.print("Nombre: ");
+                    System.out.print("Nombre del libro: ");
                     String nombre = sc.nextLine();
 
-                    System.out.print("Categoria: ");
+                    System.out.print("Categoría: ");
                     String categoria = sc.nextLine();
 
-                    System.out.print("ID: ");
-                    String id = sc.nextLine();
+                    String id;
+
+                    while (true) {
+                        System.out.print("ID: ");
+                        id = sc.nextLine();
+
+                        if (tienda.buscarProducto(id) != null) {
+                            System.out.println("Error: ya existe un producto con ese ID");
+                        } else {
+                            break; // Porque el ID ya sería válido
+                        }
+                    }
 
                     System.out.print("Precio: ");
                     double precio = sc.nextDouble();
@@ -46,7 +60,7 @@ public class ProyectoEstructurasdeDatos {
                     System.out.print("Cantidad: ");
                     int cantidad = sc.nextInt();
                     sc.nextLine();
-
+                  
                     ArrayList<String> imagenes = new ArrayList<>(); // ArrayList que guarda las imagenes que desee agregar a cada NodoProducto
 
                     System.out.print("Cuántas imágenes desea agregar? ");
@@ -59,120 +73,113 @@ public class ProyectoEstructurasdeDatos {
                         imagenes.add(sc.nextLine());
                     }
                     
-                    lista.insertarNodoInicio(nombre, categoria, id, precio, cantidad, imagenes);
+                    //Crear el producto y agregarlo al inventario
+                    NodoProducto producto = new NodoProducto (nombre, categoria, id, precio, cantidad, imagenes);
+                    tienda.agregarProducto(producto);
+                    
                     break;
                     
-                // Insertar al final
+                // Mostrar inventario
                 case 2:
-                    System.out.print("Nombre: ");
-                    String nombreF = sc.nextLine();
-
-                    System.out.print("Categoria: ");
-                    String categoriaF = sc.nextLine();
-
-                    System.out.print("ID: ");
-                    String idF = sc.nextLine();
-
-                    System.out.print("Precio: ");
-                    double precioF = sc.nextDouble();
-
-                    System.out.print("Cantidad: ");
-                    int cantidadF = sc.nextInt();
-                    sc.nextLine();
-
-                    ArrayList<String> imagenesF = new ArrayList<>();
-
-                    System.out.print("Cuántas imágenes desea agregar? ");
-                    int numImgF = sc.nextInt();
-                    sc.nextLine();
-
-                    for (int i = 0; i < numImgF; i++) {
-                        System.out.print("Imagen " + (i+1) + ": ");
-                        imagenesF.add(sc.nextLine());
-                    }
-                    
-                    lista.insertarNodoFInal(nombreF, categoriaF, idF, precioF, cantidadF, imagenesF);
+                    tienda.mostrarInventario();
                     break;
                   
-                // Mostrar lista
+                // Agregar cliente y llenar el carrito
                 case 3: 
-                    lista.mostrarLista();
-                    break;
-                
-                // Buscar por ID
-                case 4:
-                    System.out.println("Ingrese el ID que desea buscar: ");
-                    id = sc.nextLine();
-                    lista.buscar(id);
-                    break;
-                
-                // Eliminar libro
-                case 5:
-                    System.out.println("Ingrese el ID que desea eliminar: ");
-                    id = sc.nextLine();
-                    lista.eliminar(id);
-                    break;
-                
-                // Modificar libro
-                case 6:
-                    System.out.println("Ingrese el ID que desea modificar: ");
-                    id = sc.nextLine();
+                    System.out.println("Nombre del cliente: ");
+                    String nombreCliente = sc.nextLine();
                     
-                    // Verificamos si el libro existe
-                    NodoProducto producto = lista.buscar(id);
+                    String idCliente;
 
-                    if (producto == null) {
-                        break; // volver al menú principal
-                    }
-                    boolean seguir = true; // Variable para controlar el ciclo while de modificar
-                    
-                    // Bucle del menú para modificar
-                    while (seguir){
-                        System.out.println("Cuál dato desea modificar? \n" +
-                                            "1. Nombre \n" +
-                                            "2. Categoría \n" +
-                                            "3. Precio \n" +
-                                            "4. Cantidad \n" +
-                                            "5. Volver al menú principal \n");
-                        int opc = sc.nextInt();
-                        sc.nextLine();
-                        
-                        switch(opc){
-                        
-                            case 1:
-                                System.out.print("Nuevo nombre: ");
-                                String nuevoNombre = sc.nextLine();
-                                
-                                lista.modificarNombre(id,nuevoNombre);
-                                break;
-                            case 2:
-                                System.out.print("Nueva categoría: ");
-                                String nuevaCategoria = sc.nextLine();
-                                
-                                lista.modificarCategoria(id, nuevaCategoria);
-                                break;
-                            case 3:
-                                System.out.print("Nuevo precio: ");
-                                double nuevoPrecio = sc.nextDouble();
-                                sc.nextLine();
-                                
-                                lista.modificarPrecio(id, nuevoPrecio);
-                                break;
-                            case 4:
-                                System.out.print("Nueva cantidad: ");
-                                int nuevaCantidad= sc.nextInt();
-                                sc.nextLine();
+                    while (true) {
+                        System.out.print("ID del cliente: ");
+                        idCliente = sc.nextLine();
 
-                                lista.modificarCantidad(id, nuevaCantidad);
-                                break;
-                            case 5:
-                                System.out.print("Volviendo al menú principal...\n");
-                                seguir = false; // Para salir del bucle de modificar
-                                break;
-                            default:
-                                System.out.print("Opción inválida, intente nuevamente. \n");
-                                break;
+                        if (tienda.buscarClienteEnCola(idCliente) != null) {
+                            System.out.println("Error: ya existe un cliente con ese ID");
+                        } else {
+                            break; // ID válido
                         }
+                    }
+                    
+                    System.out.println("Prioridad (1 Básico, 2 Afiliado, 3 Premium): ");
+                    int prioridad = sc.nextInt();
+                    sc.nextLine();
+                    
+                    // Crear el cliente
+                    Cliente cliente = new Cliente(nombreCliente, idCliente, prioridad);
+                    
+                    // Llenar carrito
+                    boolean agregarMas = true;
+                    
+                    while (agregarMas){
+                        System.out.println("INVENTARIO DISPONIBLE\n");
+                        tienda.mostrarInventario();
+                        
+                        System.out.print("Ingrese el ID del producto que desea agregar: ");
+                        String idProducto = sc.nextLine();
+                        
+                        NodoProducto prod = tienda.buscarProducto(idProducto);
+                        
+                        if(prod == null){
+                           System.out.println("Producto no encontrado\n");
+                        } else {
+                            System.out.print("Cantidad: ");
+                            int cant = sc.nextInt();
+                            sc.nextLine();
+                        
+                            // Validacion de stock
+                            if (cant > prod.getCantidad()) {
+                                System.out.println("Error: no hay suficiente stock disponible");
+                                continue; // vuelve a pedir otro producto
+                            }
+
+                            // Crear copia del producto para el carrito
+                            NodoProducto copia = new NodoProducto(
+                                    prod.getNombre(),
+                                    prod.getCategoria(),
+                                    prod.getId(),
+                                    prod.getPrecio(),
+                                    cant,
+                                    prod.getListaImagenes()
+                            );
+
+                            // Agregar al carrito del cliente
+                            cliente.getCarrito().insertarNodoFInal(
+                                    copia.getNombre(),
+                                    copia.getCategoria(),
+                                    copia.getId(),
+                                    copia.getPrecio(),
+                                    copia.getCantidad(),
+                                    copia.getListaImagenes()
+                            );
+
+                            System.out.println("Producto agregado al carrito\n");
+                        }
+
+                        System.out.print("¿Desea agregar otro producto? (s/n): ");
+                        String resp = sc.nextLine();
+
+                        if (resp.equalsIgnoreCase("n")) {
+                            agregarMas = false;
+                        }
+                    }
+                    // Agregar cliente a la cola
+                    tienda.agregarCliente(cliente);
+                    
+                    break;
+                
+                // Ver Cola
+                case 4:
+                    tienda.mostrarClienteFrente();
+                    break;
+                
+                // Atender Cliente
+                case 5:
+                    Cliente atendido = tienda.atenderSiguienteCliente();
+                    
+                    if (atendido != null){
+                        tienda.generarFactura(atendido);
                     }
                     break;
                 
